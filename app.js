36d767786a81478e7c174ddcd67c2a72a04d2147
robertgeica <geicarobert@gmail.com>
2022-03-27 22:09:27 +0300

    update styles

--- app.js
+++ app.js
@@ -50,38 +50,36 @@ const renderGameBoard = (board) => {
       (cell, cellIndex) =>
         `<div id="${rowIndex}${cellIndex}" class="cell ${
           hasHittedShip(cell) ? "hit" : "not-hit"
-        }" onclick="onCellClick(this)">${hasShip(
-          cell
-        )} ${rowIndex} ${cellIndex}</div>`
+        }" onclick="onCellClick(this)">${
+          hasShip(cell) ? "ship" : ""
+        } ${rowIndex} ${cellIndex}</div>`
     );
-    const rowDOM = `<div class="row" ondrop="drop(event)" ondragover="allowDrop(event)">${cells.join(
-      "\n"
-    )}</div>`;
-
+    const rowDOM = `<div class="row" ondrop="drop(event)" ondragover="allowDrop(event)">
+    ${cells.join("\n")}</div>`;
     return rowDOM;
   });
-  boardDOM.innerHTML += boardDOM.innerHTML + rowsDOM;
+
+  boardDOM.innerHTML += boardDOM.innerHTML + rowsDOM.join("\n");
 };
 renderGameBoard(playerBoard);
-
 
 const renderShips = (ships) => {
   const shipsContainerDOM = document.getElementById("ships");
   shipsContainerDOM.innerHTML = "";
 
-  let shipsDOM;
+  let shipsDOM = "";
 
   for (let ship in ships) {
-    let shipCells;
+    let shipCells = "";
     for (let i = 1; i <= ships[ship].cells; i++) {
       shipCells += `<div class="ship-cell">${i}</div>`;
     }
+
     shipsDOM += `<div class="ship" data-cells=${ships[ship].cells} data-name=${ships[ship].name} draggable="true" ondragstart="drag(event)">${shipCells}</div>`;
   }
   shipsContainerDOM.innerHTML += shipsContainerDOM.innerHTML + shipsDOM;
 };
 renderShips(playerShips);
-
 
 const onCellClick = (cell) => {
   if (!gameState.playerTurn) return console.log("wait for your turn");
@@ -99,7 +97,6 @@ const onCellClick = (cell) => {
   gameState.playerTurn = false;
   // TODO: must call a function for computer hit
 };
-
 
 const placeShip = (ship, rowIndex, cellIndex) => {
   const { shipName, shipCells } = ship;
@@ -136,21 +133,23 @@ const playerReady = () => {
   }
 };
 
-const allowDrop = e => e.preventDefault();
+const allowDrop = (e) => e.preventDefault();
 
-const drag = e => {
+const drag = (e) => {
   const shipCells = e.target.dataset.cells;
   const shipName = e.target.dataset.name;
   e.dataTransfer.setData("shipCells", shipCells);
   e.dataTransfer.setData("shipName", shipName);
-}
+};
 
-const drop = e => {
+const drop = (e) => {
   e.preventDefault();
   const shipCells = parseInt(e.dataTransfer.getData("shipCells"));
   const shipName = e.dataTransfer.getData("shipName");
   const row = parseInt(e.target.id[0]);
   const cell = parseInt(e.target.id[1]);
-  
+
   placeShip({ shipCells, shipName }, row, cell);
-}
+};
+
+// TODO: generate and render computer board and ships
