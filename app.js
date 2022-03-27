17611fa652b97404e773492ea62dd520ce32cc98
robertgeica <geicarobert@gmail.com>
2022-03-27 13:25:25 +0300

    add board and ships template

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 // utils functions
 const cloneObject = object => JSON.parse(JSON.stringify(object));
 
+// board and ships template
+const initialGameBoardTemplate = [
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+  [[], [], [], [], [], [], [], [], [], []],
+];
+
+let initialShipsTemplate = [
+  { name: "carrier", cells: 5 },
+  { name: "battleship", cells: 4 },
+  { name: "destroyer", cells: 3 },
+  { name: "submarine", cells: 3 },
+  { name: "patrol", cells: 2 },
+];
